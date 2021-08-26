fdb6851ee079f7ba0e8eee235f3be89088a15ca6
Fanboy321 <87923788+Fanboy321@users.noreply.github.com>
2021-08-26 16:31:07 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -120,7 +120,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '918921483992@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./stickers/mention.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['Hi','set','vanidi']
+const array = ['Hi','Hlo','bot','Mrng','set','vanidi']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
